b4890869663aba560ea488f1dacca3524b532ecc
amo-grau <oscaramog@gmail.com>
2025-01-08 10:53:56 +0100

    replace remove button temp with query

--- GuildedRose/UI/ItemModel.cs
+++ GuildedRose/UI/ItemModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace GuildedRose.UI
 {
@@ -14,21 +15,24 @@ namespace GuildedRose.UI
 
         private ItemModel(string name, string sellIn)
         {
-            var removeButton = new Button() { Name = name + "RemoveButton", Text = "Remove", AutoSize = true };
-            removeButton.Click += (_, _) => NotifyListeners();
-
-            displayedProperties =
-                new Dictionary<DisplayedProperties, Control>()
-                {
-                        { DisplayedProperties.Name, new TextBox { Name = name, Text = name, ReadOnly = true } },
-                        { DisplayedProperties.SellIn, new TextBox { Text = sellIn, ReadOnly = true } },
-                        { DisplayedProperties.RemoveButton, removeButton }
-                };
+            displayedProperties = new Dictionary<DisplayedProperties, Control>()
+            {
+                { DisplayedProperties.Name, new TextBox { Name = name, Text = name, ReadOnly = true } },
+                { DisplayedProperties.SellIn, new TextBox { Text = sellIn, ReadOnly = true } },
+                { DisplayedProperties.RemoveButton, RemoveButton(name) }
+            };
 
             modeledItem = new Item(name) with { SellIn = int.Parse(sellIn) };
         }
 
         public IReadOnlyCollection<UserRequestListener> RemoveButtonListeners { private get;  set; } = new List<UserRequestListener>();
+
+        private Button RemoveButton(string itemName)
+        {
+            var removeButton = new Button() { Name = itemName + "RemoveButton", Text = "Remove", AutoSize = true };
+            removeButton.Click += (_, _) => NotifyListeners();
+            return removeButton;
+        }
 
         private void NotifyListeners()
         {
